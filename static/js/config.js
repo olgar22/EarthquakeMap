3650b9653aae5c2caef280c7530f0dfd1ee21053
olgaroginkin <44462350+olgar22@users.noreply.github.com>
2019-04-14 22:54:58 -0400

    updated with fresh API token

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,3 @@
 // API key
-var API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+//var API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+var API_KEY = "pk.eyJ1Ijoib2xnYTIyIiwiYSI6ImNqdWhyamhoaTB6dGkzeW54c3NxbjgzdmcifQ.LOTe4OuiyCO5So4Ktma3Tw";
